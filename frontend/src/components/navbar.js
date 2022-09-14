7f89b0111a64ba7f2c3d4e4ddcfaa1564b5de9e9
jpolvine <jpolvine1@gmail.com>
2022-09-14 14:43:11 +0300

    Fixed errors
    Added own icon and renamed some classes into className

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink className="navbar-brand" to="/">
-          <h3 class="d-inline-block align-text-middle" >TODO List</h3></NavLink>
+          <h3 className="d-inline-block align-text-middle" >TODO List</h3></NavLink>
         <button
           className="navbar-toggler"
           type="button"
